9fc8d88d187aa7305900a8f90a4287431eb51988
ablue09 <ablue09@gmail.com>
2016-03-30 15:11:40 -0400

    Added files via upload

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,7 @@
 
 using namespace std;
 
+void sneaky(int& temp);
 void output();
 
 int main()
@@ -76,8 +77,84 @@ int main()
 
 #pragma endregion
 
+#pragma region //Array pointer
+	int* pA;
+	int a[10];
+	int index;
+
+	for (index = 0; index < 10; index++)
+		a[index] = index;
+
+	pA = a;
+
+	for (index = 0; index < 10; index++)
+		cout << pA[index] << " ";
+	cout << endl;
+
+	for (index = 0; index < 10; index++)
+		pA[index] = pA[index] + 1;
+
+	for (index = 0; index < 10; index++)
+		cout << a[index] << " ";
+	cout << endl;
+#pragma endregion
+
+#pragma region //Reference Parameter in a Function
+    // Initialize a variable
+    int v = 77;
+
+    cout << "Before call to function v == " << v << endl;
+
+    sneaky(v);
+
+    cout << "After call to function v == " << v << endl;
+#pragma endregion
+
+#pragma region //Dynamic Allocation (cont...). Memory Leaks
+// Initialize two pointers
+    int *p1c, *p2c;
+
+    // Dynamically allocate one integer
+    p1c = new int;
+
+    // Initialize value of dynamically allocated integer
+    *p1c = 42;
+
+    // Set the other pointer to point to this variable as well
+    p2c = p1c;
+
+    cout << "*p1c == " << *p1c << endl;
+    cout << "*p2c == " << *p2c << endl;
+
+    // Change value of dynamically allocated variable
+    *p2c = 53;
+
+    cout << "*p1c == " << *p1c << endl;
+    cout << "*p2c == " << *p2c << endl;
+
+    // Allocate another variable dynamically
+    p1c = new int;
+
+    // Initialize value of second dynamically allocated variable
+    *p1c = 88;
+
+    cout << "*p1c == " << *p1c << endl;
+    cout << "*p2c == " << *p2c << endl;
+
+    // Free up dynamically allocated variables, to avoid a memory leak
+    delete p1c;
+    delete p2c;
+
+    cout << "Hope you got the point of this example!\n";
+
+	//int* pM;
+	//new int(9);
+	//*pM = 10;
+#pragma endregion
+
 	//system("pause");
 
+#pragma region //Display Memory Leak warning
 	if(_CrtDumpMemoryLeaks())
 	{	
 		cout << "--------------------------------" << endl;
@@ -90,8 +167,23 @@ int main()
 		cout << "------- NO memory leaks! -------" << endl;
 		cout << "--------------------------------" << endl;
 	}
+#pragma endregion
 
 	return 0;
+}
+
+/* sneaky()
+ *
+ * Demonstrates how referencess can be used to change a parameter's value.
+ *
+ * Requires: A reference to an initialized integer.
+ * Modifies: The value of the parameter.
+ */
+void sneaky(int& temp)
+{
+    temp = 99;
+    cout << "Inside function call temp == "
+         << temp << endl;
 }
 
 void output()
